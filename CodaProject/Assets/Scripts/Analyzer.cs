9a37e3777c9c1d74b8f64cdf1ddb96d92cc6c449
Chris <cloutc@rpi.edu>
2015-10-27 16:23:53 -0400

    Fixes to FFT and Welch's stuff. Still only happening on a single section of samples

--- CodaProject/Assets/Scripts/Analyzer.cs
+++ CodaProject/Assets/Scripts/Analyzer.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Analyzer : MonoBehaviour {
 
@@ -19,23 +20,39 @@ public class Analyzer : MonoBehaviour {
 		//print (samples.Length);
 		
 		//print (clip.samples/1000);
-		clip.GetData(samples, 8000);
-		print ("num samples: " + samples.Length);
+		clip.GetData(samples, 16000);
+
+		//print ("num samples: " + samples.Length);
 		
 		for(int n = 0; n < samples.Length; n++)
 		{
-			samples[n] = a0 - a1 * Mathf.Cos((2 * Mathf.PI * n)/samples.Length-1) + a2 * Mathf.Cos((4*Mathf.PI * n)/samples.Length-1) - a3 * Mathf.Cos((6 * Mathf.PI * n)/samples.Length-1);
-		}	
+			samples[n] *= a0 - a1 * Mathf.Cos((2 * Mathf.PI * n)/samples.Length-1) + a2 * Mathf.Cos((4*Mathf.PI * n)/samples.Length-1) - a3 * Mathf.Cos((6 * Mathf.PI * n)/samples.Length-1);
+		}
 		
 		FFT2 test = new FFT2();
 		test.init(10);
-		test.run (samples, new float[samples.Length], false);
-		
+		double[] double_samples = samples.ToList ().ConvertAll<double>(new System.Converter<float, double>(f2d)).ToArray ();
+		test.run (double_samples, new double[samples.Length], false);
+		//samples = double_samples.ToList ().ConvertAll<float> (new System.Converter<double, float> (d2f)).ToArray ();
+
+		double avg = samples.Average ();	
+		print (avg);
+
 		for(int i = 1; i < samples.Length-1; i++)
 		{
 			Debug.DrawLine(new Vector3(Mathf.Log(i - 1), samples[i] + 10, 0), new Vector3(Mathf.Log(i), samples[i + 1] + 10, 0), Color.red);
 		}
 		
+	}
+
+	public static double f2d(float f)
+	{
+		return (double)f;
+	}
+
+	public static float d2f(double d)
+	{
+		return (float)d;
 	}
 	
 }
