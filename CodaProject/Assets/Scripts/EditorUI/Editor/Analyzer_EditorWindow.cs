b4c8661614faea7d158cbf3a804bca22594e0331
Chris <cloutc@rpi.edu>
2015-11-03 17:07:58 -0500

    Beatmap now being put into XML, beat information drawn with waveform

--- CodaProject/Assets/Scripts/EditorUI/Editor/Analyzer_EditorWindow.cs
+++ CodaProject/Assets/Scripts/EditorUI/Editor/Analyzer_EditorWindow.cs
@@ -43,18 +43,20 @@ public class Analyzer_EditorWindow : EditorWindow {
         if (analysisControlWindow.triggerAnalysis == true) {
             analysisControlWindow.triggerAnalysis = false;
             double[] data = analyzer.ProcessAudio(analysisControlWindow.musicToAnalyze);
-            BeatMapToFile(data, analysisControlWindow.musicToAnalyze.name);
+            BeatMap beats = analyzer.AnalyzeData(data, analysisControlWindow.musicToAnalyze);
+            BeatMapToFile(beats, analysisControlWindow.musicToAnalyze.name);
             waveformMarkupWindow.waveform = data;
+            waveformMarkupWindow.beatmap = beats;
         }
     }
 
-    private void BeatMapToFile(double[] data, string name)
+    private void BeatMapToFile(BeatMap beats, string name)
     {
         BeatMapWriter writer = new BeatMapWriter();
-        BeatMap map = new BeatMap(name);
+        //BeatMap map = new BeatMap(name);
         // this is a test using a dummy object
-        map.AddBeat(1, 3f, 5f);
-        writer.WriteBeatMap(map);
+        //map.AddBeat(1, 3f, 5f);
+        writer.WriteBeatMap(beats);
     }
 
     private void HandleWindowInstantiation() {
