533729b8f5689bc418c4ffe37bf7f600b93d929b
unknown <cirigj@SA2411-PC16.win.rpi.edu>
2015-12-02 23:52:09 -0500

    ++ Added Rave Speaker ++ Added Rave Material

---
+++ CodaProject/Assets/Coda/Demo/Scripts/RaveSpeaker.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Coda;
+using JBirdEngine;
+
+public class RaveSpeaker : MusicBehaviour {
+
+    public Material raveMat;
+    public float startHue;
+    public float baseSize;
+    public float extraSize;
+    public float centerRigidity;
+    public float timePrecison;
+    private float _huePerSecond;
+    public Transform diaphragm;
+    public Transform center;
+
+    protected override void Start () {
+        base.Start();
+        DetermineHuePerSecond();
+    }
+
+    protected override void Update () {
+        raveMat.SetColor("_Color", raveMat.color.ToHSV().ShiftHue(_huePerSecond * Time.deltaTime).ToColor());
+        if (Maestro.current.closestBeatIndex % 2 == 0) {
+            float timeDiff;
+            if (Maestro.current.IsOnBeat(timePrecison, out timeDiff)) {
+                diaphragm.localScale = Vector3.one * (baseSize + extraSize * ((timePrecison - timeDiff) / timePrecison));
+                center.localScale = new Vector3 (1f * (baseSize + extraSize * ((timePrecison - timeDiff) / timePrecison) * (1f - centerRigidity)), 1f, 1f);
+            }
+            else {
+                diaphragm.localScale = Vector3.one * baseSize;
+                center.localEulerAngles = Vector3.one * baseSize;
+            }
+        }
+    }
+
+    public override void OnBeat () {
+        DetermineHuePerSecond();
+    }
+
+    void DetermineHuePerSecond () {
+        _huePerSecond = 30f / Maestro.current.timeUntilNextBeat;
+    }
+
+}
