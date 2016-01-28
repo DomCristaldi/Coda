e29cc8b1623b26c3aa5b3ef90be1b2564112065e
DomCristaldi <domeniccristaldi@gmail.com>
2016-01-27 20:55:02 -0500

    ++ Made a popup window for authoring custom beatmaps

---
+++ CodaProject/Assets/Coda/EditorUI/Editor/AuthorBeatmap_EditorWindow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+using Coda;
+
+public class AuthorBeatmap_EditorWindow : EditorWindow {
+
+    private string _beatmapName;
+    private float _beatmapBPM;
+    private int _beatmapNumBeats;
+
+    [MenuItem("Coda/Beatmap Author")]
+    static void CreateAuthoringWindowUtility() {
+        //AuthorBeatmap_EditorWindow window = new AuthorBeatmap_EditorWindow();
+        AuthorBeatmap_EditorWindow window = GetWindow<AuthorBeatmap_EditorWindow>();
+        window.ShowUtility();
+    }
+
+
+    void OnGUI() {
+        EditorGUILayout.BeginVertical();
+
+        if (GUILayout.Button("Create Beatmap")) {
+            AnalyzerTools.BeatMapFromBPM(_beatmapName, _beatmapBPM, _beatmapNumBeats);
+        }
+
+        _beatmapName = EditorGUILayout.TextField("Name", _beatmapName);
+        _beatmapBPM = EditorGUILayout.FloatField("BPM", _beatmapBPM);
+        _beatmapNumBeats = EditorGUILayout.IntField("Number of Beats", _beatmapNumBeats);
+
+
+        EditorGUILayout.EndVertical();
+    }
+
+}
